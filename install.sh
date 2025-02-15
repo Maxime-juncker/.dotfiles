999b2408940375ffe471f9942700f5e68ffa6302
Maxime Juncker <mjuncker@z3r8p1.42lyon.fr>
2025-02-15 12:57:09 +0100

    added vscode config + install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -fns ~/.dotfiles/.zshrc ~/.zshrc
+ln -fns ~./dotfiles/.gitconfig ~/.gitconfig
+ln -fns ~/.dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+ln -fns ~/.dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
