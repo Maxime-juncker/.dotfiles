fe7822090eeecee55ae4b9ce592955c1759ce1fc
Maxime Juncker <maxime.junck@gmail.com>
2025-08-28 09:57:11 +0200

    added cpptools depedencies

--- nvim/lua/config/mason.lua
+++ nvim/lua/config/mason.lua
@@ -2,6 +2,7 @@ require("mason-tool-installer").setup({
 	ensure_installed = {
 		"clangd",       -- C/C++ language server
 		"codelldb",
+		"cpptools"		-- dap debuger
 	},
 	auto_update = true,
 	run_on_start = true,
