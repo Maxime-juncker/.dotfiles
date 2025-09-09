2e57bca455f797d5eb71b1ba019efd9170ddff37
Maxime Juncker <maxime.junck@gmail.com>
2025-09-09 17:07:54 +0200

    remove useless code

--- setup.sh
+++ setup.sh
@@ -14,8 +14,6 @@ printf "setting up config files\n"
 ln -fns ~/.dotfiles/.zshrc ~/.zshrc
 ln -fns ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -fns ~/.dotfiles/.gitconfig ~/.gitconfig
-# ln -fns ~/.dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
-# ln -fns ~/.dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 
 # nvim
 ln -fns ~/.dotfiles/nvim ~/.config/
