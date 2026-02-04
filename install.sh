3d60344e9fc1b2c4fc94b1543f34975f14b418e4
Maxime Juncker <maxime.junck@gmail.com>
2026-02-04 13:49:07 +0100

    install script

--- install.sh
+++ install.sh
@@ -7,15 +7,25 @@ sudo apt-get install valgrind -y
 sudo apt-get install ripgrep -y
 sudo apt-get install fd-find -y
 sudo apt-get install cppman -y
-sudo apt-get install build-essential -y
+sudo apt install build-essential procps curl file git -y
 
 # install zsh
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+sudo apt install tmux -y
+sudo apt install kitty -y
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+[ -d /home/linuxbrew/.linuxbrew ] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+brew install neovim
+
+
+echo "setup symbolic links"
 # set conf files
-ln -s ./.gitconfig $HOME/.gitconfig
-ln -s ./.tmux $HOME/.tmux
-ln -s ./.zshrc $HOME/.zshrc
-ln -s ./nvim $HOME/.config/nvim
+ln -fns $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
+ln -fns $HOME/.dotfiles/.tmux $HOME/.tmux
+ln -fns $HOME/.dotfiles/.zshrc $HOME/.zshrc
+ln -fns $HOME/.dotfiles/nvim $HOME/.config/nvim
 
